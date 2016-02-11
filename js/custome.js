5b3253511785c20f8ecf1c010355e4799c437232
Adam Samaha <adamsamahaa@gmail.com>
2016-02-11 10:09:26 -0800

    add files

--- js/custome.js
+++ js/custome.js
@@ -37,48 +37,48 @@
             });
         });
         });
-// //about me toggle
-// $(function() {
-// $( "#btn-bg" ).click(function() {
-// $("#myskills").hide();
-// $( "#background" ).slideToggle('slow');
-// });
-// });
-// $(function() {
-// $( "#btn-skls" ).click(function() {
-// $( "#myskills" ).slideToggle('slow');
-// $("#background").hide();
-// });
-// });
-// // Projects Buttons
-//     $(".button-fill").hover(function () {
-//     $(this).children(".button-inside").addClass('full');
-//     }, function() {
-//     $(this).children(".button-inside").removeClass('full');
-//     });
-// //animation
-// var $animation_elements = $('.animation-element');
-// var $window = $(window);
+//about me toggle
+$(function() {
+$( "#btn-bg" ).click(function() {
+$("#myskills").hide();
+$( "#background" ).slideToggle('slow');
+});
+});
+$(function() {
+$( "#btn-skls" ).click(function() {
+$( "#myskills" ).slideToggle('slow');
+$("#background").hide();
+});
+});
+// Projects Buttons
+    $(".button-fill").hover(function () {
+    $(this).children(".button-inside").addClass('full');
+    }, function() {
+    $(this).children(".button-inside").removeClass('full');
+    });
+//animation
+var $animation_elements = $('.animation-element');
+var $window = $(window);
 
-// function check_if_in_view() {
-//   var window_height = $window.height();
-//   var window_top_position = $window.scrollTop();
-//   var window_bottom_position = (window_top_position + window_height);
+function check_if_in_view() {
+  var window_height = $window.height();
+  var window_top_position = $window.scrollTop();
+  var window_bottom_position = (window_top_position + window_height);
 
-//   $.each($animation_elements, function() {
-//     var $element = $(this);
-//     var element_height = $element.outerHeight();
-//     var element_top_position = $element.offset().top;
-//     var element_bottom_position = (element_top_position + element_height);
+  $.each($animation_elements, function() {
+    var $element = $(this);
+    var element_height = $element.outerHeight();
+    var element_top_position = $element.offset().top;
+    var element_bottom_position = (element_top_position + element_height);
 
-//     //check to see if this current container is within viewport
-//     if ((element_bottom_position >= window_top_position) &&
-//       (element_top_position <= window_bottom_position)) {
-//       $element.addClass('in-view');
-//     } else {
-//       $element.removeClass('in-view');
-//     }
-//   });
-// }
-// $window.on('scroll resize', check_if_in_view);
-// $window.trigger('scroll');
+    //check to see if this current container is within viewport
+    if ((element_bottom_position >= window_top_position) &&
+      (element_top_position <= window_bottom_position)) {
+      $element.addClass('in-view');
+    } else {
+      $element.removeClass('in-view');
+    }
+  });
+}
+$window.on('scroll resize', check_if_in_view);
+$window.trigger('scroll');
